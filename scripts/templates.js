a2fd956d4d9ce9f24abc3def4503317fa72b52f2
Patrick Wagner <patrickwagner2607@gmail.com>
2026-09-13 11:04:16 +0200

    feat: bookcard template without commets and likefunction

---
+++ scripts/templates.js
@@ -0,0 +1,94 @@
+function templateBookcard(i) {
+    return /*html*/`
+        <section class="BookCard">
+            <header class="BookHeader">
+                <h2>
+                    ${books[i].name}
+                </h2>
+            </header>
+            <figure class="BookImage">
+                <img src="${books[i].cover}" alt="${books[i].name} Cover">
+            </figure>
+            <main class="BookInfo">
+                <section class="BookPriceLikes"">
+                    <p class=" BookPrice">
+                    ${books[i].price.toFixed(2).replace(".",",")} €
+                    </p>
+                    <p class="BookLikes">
+                        ${books[i].likes}
+                        <img class="NoLike" src="./assets/icons/heart_outline.png" alt="Hohles Herz">
+                        <img class="YesLike" src="./assets/icons/heart_filled.png" alt="Ausgefülltes Herz">
+                    </p>
+                </section>
+                <table class="BookData">
+                    <tr>
+                        <th>
+                            Author
+                        </th>
+                        <td>
+                            : ${books[i].author}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Erscheinungsjahr
+                        </th>
+                        <td>
+                            : ${books[i].publishedYear}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Genre
+                        </th>
+                        <td>
+                            : ${books[i].genre}
+                        </td>
+                    </tr>
+                </table>
+            </main>
+            <footer class="BookFooter">
+                <h3>
+                    Kommentare:
+                </h3>
+                <section class="BookCommentarySection">
+                    <table>
+                        <tr>
+                            <th>
+                                [Leser123]
+                            </th>
+                            <td>
+                                Ein faszinierendes Abenteuerbuch, das mich von der ersten SEite an gefesselt hat.
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                [Leser123]
+                            </th>
+                            <td>
+                                Ein faszinierendes Abenteuerbuch, das mich von der ersten SEite an gefesselt hat.
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                [Leser123]
+                            </th>
+                            <td>
+                                Ein faszinierendes Abenteuerbuch, das mich von der ersten SEite an gefesselt hat.
+                            </td>
+                        </tr>
+                    </table>
+                </section>
+                <section class="BookCommentaryInput">
+                    <input type="text" value=" Schreibe dein Kommentar ....">
+                    <button aria-label="send Commentary" class="CommentaryButton">
+                        <img class="CommentaryArrow CommentaryArrowOutline" aria-label="Kommentar-Button"
+                            src="./assets/icons/paper_plane_outline_light.png" alt="Kommentar Abschicken Button">
+                        <img class="CommentaryArrow CommentaryArrowFilled" aria-label="Kommentar-Button"
+                            src="./assets/icons/paper_plane_filled_light.png" alt="Kommentar Abschicken Button">
+                    </button>
+                </section>
+            </footer>
+        </section>
+    `
+}
